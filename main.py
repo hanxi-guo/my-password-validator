feefccd3f1d5350e467ace9f03f8227111ddf794
Hanxi <152403642+hanxi-guo@users.noreply.github.com>
2025-03-25 13:08:58 -0400

    fix: try to fix the get 501 error

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import flask
+import re
 
 
 # TODO: change this to your academic email
-AUTHOR = "lumbroso@seas.upenn.edu"
+AUTHOR = "syensi@seas.upenn.edu"
 
 
 app = flask.Flask(__name__)
@@ -20,10 +21,27 @@ def hello():
 # It is not yet implemented, and will return HTTP 501 in all situations
 
 
-@app.route("/v1/checkPassword", methods=["POST"])
+@app.route("/checkPassword", methods=["POST"])
 def check_password():
     data = flask.request.get_json() or {}
     pw = data.get("password", "")
 
     # FIXME: to be implemented
-    return flask.jsonify({"valid": False, "reason": "Not implemented"}), 501
+    if len(pw) < 8:
+        return flask.jsonify({"valid": False, "reason": "Password too short"}), 400
+    if not len(re.findall(r"[A-Z]", pw)) < 2:
+        if re.search(r"[A-Z]", pw):
+            return flask.jsonify({"valid": False, "reason": "Not enough uppercase letter"}), 400
+        else:
+             return flask.jsonify({"valid": False, "reason": "Missing uppercase letter"}), 400
+    if not len(re.findall(r"[a-z]", pw)) < 2:
+        if re.search(r"[a-z]", pw):
+            return flask.jsonify({"valid": False, "reason": "Not enough lowercase letter"}), 400
+        else:
+             return flask.jsonify({"valid": False, "reason": "Missing lowercase letter"}), 400
+    if not re.search(r"[0-9]", pw):
+        return flask.jsonify({"valid": False, "reason": "Missing digit"}), 400
+    if not re.search(r"[!@#$%^&*]", pw):
+        return flask.jsonify({"valid": False, "reason": "Missing special character"}), 400
+
+    return flask.jsonify({"valid": True, "reason": "Password is strong"}), 200
